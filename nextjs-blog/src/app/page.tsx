b76e077b254b096b021fa970a35b51f1d028fdd9
sayyidtrq <sayyidthariq02@gmail.com>
2023-11-08 16:49:15 +0700

    ssiuu

--- nextjs-blog/src/app/page.tsx
+++ nextjs-blog/src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <nav className='w-full h-20 flex items-center justify-between px-5 shadow-2xl'>
+    <main className="bg-white flex min-h-screen flex-col items-center">
+      <nav className='w-full h-20 flex items-center justify-between px-5 shadow-md'>
         <p className='text 2xl font-bold cursor-pointer '> RISTEK </p>
         <button className=' text-gray-500  pl-4 pr-10 rounded-md hover:text-blue-700 '>
           Homepage
@@ -25,9 +25,15 @@ export default function Home() {
           Welcome, Sayyid Thariq Gilang Muttaqien
         </button>
       </nav>
-      <div className='bg-gray-400 h-screen w-full flex justify-center py-20 '>
-        <p className='text-[#1A202C] text-[32px] font-bold'>Sayyid Thariq Gilang Muttaqien</p>
-        <p className='text-[#1A202C] text-[16px] font-sans '> Class of 2023 </p>
+      <div className='w-full h-screen flex  justify-center py-[60px]'>
+      <div className='w-full flex justify-center gap-9'>
+        <div className='w-40 h-40 rounded-full border border-black '></div>
+        <div className='items-center'>
+        <p className='text-[#1A202C] text-[32px] font-bold mt-8'>Sayyid Thariq Gilang Muttaqien </p>
+        <p className='text-[#1A202C] text [16px] font - bold m-15'>Edit Profile</p>
+        <p className='text-[#1A202C] text-[18px] font-sans '>Class of 2023</p>
+        </div>
+        </div>
       </div>
     </main>
   )
